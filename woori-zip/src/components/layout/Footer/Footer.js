adfcbe4f86757786b3a2a41a242d7e270792a061
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-13 17:48:11 +0900

    [feat/#13]: 메인페이지 섹션, 푸터 초안

--- woori-zip/src/components/layout/Footer/Footer.js
+++ woori-zip/src/components/layout/Footer/Footer.js
@@ -1,15 +1,28 @@
-// components/layout/Footer/Footer.js
-import './Footer.css';
+import styles from './Footer.module.css';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <div className="footer-wrapper">
-      <footer className="footer">
-        <div className="footer-content">
-          <span className="copyright">©</span>
-          <span className="brand">fisa_crime_watch's</span>
-          <span className="divider">·</span>
-          <span className="brand-service">Woori Zip</span>
+    <div className={styles.footerWrapper}>
+      <footer className={styles.footer}>
+        <div className={styles.footerContent}>
+          <div className={styles.footerLinks}>
+            <a href="#" className={styles.footerLink}>이용약관</a>
+            <span className={styles.divider}>|</span>
+            <a href="#" className={`${styles.footerLink} ${styles.footerLinkBold}`}>개인정보처리방침</a>
+            <span className={styles.divider}>|</span>
+            <a href="#" className={styles.footerLink}>자주묻는질문</a>
+          </div>
+          <div className={styles.footerCompany}>
+            <span className={styles.footerCompanySpan}>상호: (주)우리집</span>
+            <span className={styles.divider}>|</span>
+            <span className={styles.footerCompanySpan}>대표: 피사범대</span>
+            <span className={styles.divider}>|</span>
+            <span className={styles.footerCompanySpan}>이메일 문의: woorizip2024@gmail.com</span>
+          </div>
+          <div className={styles.footerCopyright}>
+            Copyright © WOORIZIP. All rights reserved.
+          </div>
         </div>
       </footer>
     </div>
