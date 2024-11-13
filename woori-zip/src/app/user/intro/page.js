028bc5a5c25882f844aa58c609210747216e330d
Jieun-KWAK <jieun47631@gmail.com>
2024-11-13 18:30:46 +0900

    [Feat/#23] login agreement register page

---
+++ woori-zip/src/app/user/intro/page.js
@@ -0,0 +1,11 @@
+import LoginIntro from "@/components/domains/user/intro/LoginIntro.js"
+
+function Intro() {
+  return (
+    <div>
+        <LoginIntro />
+    </div>
+  )
+}
+
+export default Intro;
