2cba42acacc32e6b50a0104dff39571084097ea0
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-11-13 19:22:13 +0900

    [FEAT/#13]: 메인페이지 웹뷰 퍼블리싱 초안

--- woori-zip/src/components/layout/Footer/Footer.js
+++ woori-zip/src/components/layout/Footer/Footer.js
@@ -2,29 +2,29 @@ import styles from './Footer.module.css';
 import Link from 'next/link';
 
 export default function Footer() {
-  return (
-    <div className={styles.footerWrapper}>
-      <footer className={styles.footer}>
-        <div className={styles.footerContent}>
-          <div className={styles.footerLinks}>
-            <a href="#" className={styles.footerLink}>이용약관</a>
-            <span className={styles.divider}>|</span>
-            <a href="#" className={`${styles.footerLink} ${styles.footerLinkBold}`}>개인정보처리방침</a>
-            <span className={styles.divider}>|</span>
-            <a href="#" className={styles.footerLink}>자주묻는질문</a>
-          </div>
-          <div className={styles.footerCompany}>
-            <span className={styles.footerCompanySpan}>상호: (주)우리집</span>
-            <span className={styles.divider}>|</span>
-            <span className={styles.footerCompanySpan}>대표: 피사범대</span>
-            <span className={styles.divider}>|</span>
-            <span className={styles.footerCompanySpan}>이메일 문의: woorizip2024@gmail.com</span>
-          </div>
-          <div className={styles.footerCopyright}>
-            Copyright © WOORIZIP. All rights reserved.
-          </div>
-        </div>
-      </footer>
-    </div>
-  );
+ return (
+   <div className={styles.footerWrapper}>
+     <footer className={styles.footer}>
+       <div className={styles.footerContent}>
+         <div className={styles.footerTop}>
+           <span>이용약관</span>
+           <span className={styles.divider}>|</span>
+           <span>개인정보처리방침</span>
+           <span className={styles.divider}>|</span>
+           <span>자주묻는질문</span>
+         </div>
+         <div className={styles.footerBottom}>
+           <span>상호 : (주)우리집</span>
+           <span className={styles.divider}>|</span>
+           <span>대표 : 피사범대</span>
+           <span className={styles.divider}>|</span>
+           <span>이메일 문의: woorizip2024@gmail.com</span>
+         </div>
+         <div className={styles.copyright}>
+           Copyright © WOORIZIP. All rights Reserved.
+         </div>
+       </div>
+     </footer>
+   </div>
+ );
 }
