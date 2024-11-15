4bf3e1226081a1277fdb72df6a42d4e3c405e25e
min20ta <kimminjis2206@gmail.com>
2024-11-15 10:07:31 +0900

    [feat/#52] : 마이페이지 대출 웹뷰 퍼블리싱

---
+++ woori-zip/src/app/mypage/loan/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WebViewMyPageLoan from '@/components/domains/mypage/loan/WebViewMyPageLoan';
+
+
+export default function MyPageLoan() {
+  return (
+    <>
+      <WebViewMyPageLoan/>
+    </>
+  );
+};
+
