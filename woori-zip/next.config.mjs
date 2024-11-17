cc570ff336f41801e7f56992ec0307eb766d5f34
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-11-17 16:54:58 +0900

    next.config 원본 파일

--- woori-zip/next.config.mjs
+++ woori-zip/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['fisa-woorizip.s3.ap-northeast-2.amazonaws.com'],
-  },
-};
-
-export default nextConfig;
+    images: {
+      domains: ['fisa-woorizip.s3.ap-northeast-2.amazonaws.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
