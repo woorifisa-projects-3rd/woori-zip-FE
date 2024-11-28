de8cfa03c416a059d24c7190c34d2860008d8434
Jieun-KWAK <jieun47631@gmail.com>
2024-11-28 19:59:14 +0900

    [feat/#100] : user/login 기본 role 설정

--- woori-zip/src/components/domains/user/login/Tab.js
+++ woori-zip/src/components/domains/user/login/Tab.js
@@ -1,27 +1,33 @@
 'use client'
 
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Tab.module.css";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 
 export default function Tab({ tabs }) {
   const searchParams = useSearchParams();
+  const router = useRouter();
+
+  // 현재 URL에서 `role` 값을 가져오되, 없으면 기본값 1로 설정
+  const currentRole = searchParams.get('role') || "0";
+
+  // 기본값을 적용하기 위해 URL을 강제로 변경
+  useEffect(() => {
+    if (!searchParams.get('role')) {
+      router.replace(`/user/login?role=0`);
+    }
+  }, [searchParams, router]);
+
   return (
     <div className={styles.tabList}>
       <ul>
         {tabs.map((tab) => (
-          // <button
-          //   key={tab.id}
-          //   onClick={() => onTabClick(tab.id)}
-          //   className={`${styles.tabButton} ${
-          //     activeTab === tab.id ? styles.active : ""
-          //   }`}
-          // > 
-          //   {tab.label}
-          // </button>
           <li key={`tab${tab.id}`}>
-            <Link href={`/user/login?role=${tab.id}`} className={searchParams.get('role') == tab.id ? styles.active : ''}>
+            <Link
+              href={`/user/login?role=${tab.id}`}
+              className={currentRole === tab.id.toString() ? styles.active : ""}
+            >
               <span>{tab.label}</span>
             </Link>
           </li>
