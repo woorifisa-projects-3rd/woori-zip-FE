93d75c058dc0f37211015bfe1792b4459338627d
Jieun-KWAK <jieun47631@gmail.com>
2024-11-29 14:53:00 +0900

    [feat/#113] : 마이페이지 퍼블리싱

--- woori-zip/src/components/domains/mypage/MemberInfo/Member.js
+++ woori-zip/src/components/domains/mypage/MemberInfo/Member.js
@@ -3,8 +3,29 @@ import styles from './Member.module.css';
 const Member = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.message}>
-        여기는 많은 분이 알아서 채워주시길 ^^ ..
+      <div className={styles.messageBox}>
+        <div className={styles.textGroup}>
+          <p className={styles.titleText}>이름</p>
+          <p className={styles.contentText}>최종수</p>
+        </div>
+
+        <div className={styles.textGroup}>
+          <p className={styles.titleText}>생년월일</p>
+          <p className={styles.contentText}>1999.11.25</p>
+        </div>
+
+        <div className={styles.textGroup}>
+          <p className={styles.titleText}>성별</p>
+          <p className={styles.contentText}>여성</p>
+        </div>
+
+        <div className={styles.textGroup}>
+          <p className={styles.titleText}>회원등급</p>
+          <p className={styles.contentText}>골드</p>
+        </div>
+      </div>
+      <div className={styles.imgBox}>
+        <div className={styles.img}></div>
       </div>
     </div>
   );
