39ca13badbc985cc9dec63f06704e30fea73fd9e
LouiIII3 <jikdsunghuilee@naver.com>
2024-12-02 23:06:39 +0900

    [deploy/#126]: 회원가입 페이지  Suspense 컴포넌트 추가

--- woori-zip/src/app/user/(onboarding)/register/page.js
+++ woori-zip/src/app/user/(onboarding)/register/page.js
@@ -1,9 +1,15 @@
-import Agreement from "@/components/domains/user/agreement/AgreementPage"
-import RegeterIntro from "@/components/domains/user/registerIntro/RegeterIntro"
+'use client'
 
+import React, { Suspense } from 'react';
+import Agreement from '@/components/domains/user/agreement/AgreementPage';
+import RegeterIntro from '@/components/domains/user/registerIntro/RegeterIntro';
 
 function RegisterPage() {
-  return <Agreement />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Agreement />
+    </Suspense>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
