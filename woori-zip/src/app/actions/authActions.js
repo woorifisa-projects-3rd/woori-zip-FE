e0805a130c4a92b324d584bd3484b50bd8beda1f
rlfrkdms1 <rlfrkdms1@naver.com>
2024-12-02 15:43:27 +0900

    [feat/#115]: custom provider 사용하도록 변경

--- woori-zip/src/app/actions/authActions.js
+++ woori-zip/src/app/actions/authActions.js
@@ -5,19 +5,12 @@ import { AuthError } from 'next-auth';
 
 export const handleCredentialsSignin = async ({ username, password }) => {
   try {
-    await signIn('credentials', { username, password, redirectTo: '/' });
+    await signIn('woorizip', { username, password, redirectTo: '/' });
   } catch (error) {
     if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return {
-            message: 'Invalid credentials',
-          };
-        default:
-          return {
-            message: 'Something went wrong.',
-          };
-      }
+      return {
+        message: error.cause.err.message,
+      };
     }
     throw error;
   }
