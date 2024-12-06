c48f45369877d90f43dd0c7b1906d380d83b0114
Jieun-KWAK <109029034+Jieun-KWAK@users.noreply.github.com>
2024-12-06 14:55:30 +0900

    Feat/#180 중개사가 올린 집 조회 (#184)
    * [feat/#180] : 변경된 json 반환 타입 적용
    
    * [feat/#180] : 중개자가 올린 방 조회 모바일 적용
    
    * [feat/#180] : 집 이미지 추가
    
    * [feat/#180] : 중개사가 올린 집 조회 디자인 수정
    
    * [feat/#180] : css 수정

--- woori-zip/src/components/domains/realsestate/UploadedProperties/PropertyCard.js
+++ woori-zip/src/components/domains/realsestate/UploadedProperties/PropertyCard.js
@@ -14,14 +14,12 @@ const PropertyCard = ({ property, hasNext }) => {
         <li key={`property${i}`} className={styles.cardItem}>
           <div className={styles.containBox}>
             <div className={styles.imageBox}>
-              <img src={data?.imageUrl} alt="집 이미지" />
+              <img src={data?.imageUrl} alt="집 이미지"/>
             </div>
             <div className={styles.textBox}>
               <div className={styles.textTitle}>
                 <p>{data?.name}</p>
                 <p>{data?.address}</p>
-                <p>{data?.gu}</p>
-                <p>{data?.dong}</p>
               </div>
               <div className={styles.textInfo}>
                 <p>보증금 : {data?.deposit}원</p>
