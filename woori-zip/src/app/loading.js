f52d83bec413aaedbf31adf2a07a9788af93cca5
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2024-12-09 11:57:02 +0900

    Feat/#189: loading.js 수정

--- woori-zip/src/app/loading.js
+++ woori-zip/src/app/loading.js
@@ -1,14 +1,39 @@
-"use client"; // 이 지시어를 추가합니다.
+"use client";
 
-import React from "react";
-import Lottie from "lottie-react";
-import loadingAnimation from "./loading-animation.json"; // Lottie JSON 파일 경로
+import dynamic from 'next/dynamic';
+
+const Lottie = dynamic(() => import('lottie-react'), {
+ ssr: false,
+ loading: () => <p>Loading...</p>
+});
+
+import loadingAnimation from "./loading-animation.json";
 
 export default function Loading() {
-  return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-      <Lottie animationData={loadingAnimation} loop={true} style={{ width: 150, height: 150 }} />
-      <p style={{ marginTop: "20px", fontSize: "18px", color: "#374151" }}>페이지를 불러오는 중입니다...</p>
-    </div>
-  );
+ return (
+   <div 
+     style={{ 
+       display: "flex", 
+       flexDirection: "column", 
+       alignItems: "center", 
+       justifyContent: "center", 
+       height: "100vh" 
+     }}
+   >
+     <Lottie 
+       animationData={loadingAnimation} 
+       loop={true} 
+       style={{ width: 150, height: 150 }}
+     />
+     <p 
+       style={{ 
+         marginTop: "20px", 
+         fontSize: "18px", 
+         color: "#374151" 
+       }}
+     >
+       페이지를 불러오는 중입니다...
+     </p>
+   </div>
+ );
 }
